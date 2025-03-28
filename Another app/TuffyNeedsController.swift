8b7cbc26a04fb5f29be0f325e862c66a2e643b37
Kevin Henderson <kevinhenderson317@csu.fullerton.edu>
2025-03-27 22:05:26 -0700

    Added clicks label in upgrade shop, added view controller for tuffys shop where you can buy cosmetics, food, water, and the key to play certain minigames

---
+++ Another app/TuffyNeedsController.swift
@@ -0,0 +1,29 @@
+//
+//  TuffyNeedsController.swift
+//  Tuffy Tapper
+//
+//  Created by Kevin Henderson on 3/27/25.
+//
+
+import UIKit
+
+class TuffyNeedsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
